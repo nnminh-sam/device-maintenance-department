96af75d79c227618fb4a99165748136e1f0d1ddc
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-07-23 23:13:05 +0700

    feat: Initialize project

---
+++ src/main/java/ptithcm/device_maintenance/DeviceMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package ptithcm.device_maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ptithcm/device_maintenance/DeviceMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package ptithcm.device_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
